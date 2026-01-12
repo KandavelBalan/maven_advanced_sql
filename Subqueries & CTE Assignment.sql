740343a2f172c02e31e667acdb33ee043916a209
kanda <kandavelece906@gmail.com>
2026-01-12 10:11:37 +0530

    updated subqueries & CTE queries 10/01/2026 EOD

--- Subqueries & CTE Assignment.sql
+++ Subqueries & CTE Assignment.sql
@@ -33,40 +33,109 @@ ORDER BY unit_price DESC;
 
 
 -- All factories and products
+SELECT * FROM products;
 
 
 -- All factories and their total number of products
-
+SELECT factory, COUNT(*) FROM products
+GROUP BY factory;
 
 -- Final query with subqueries
+SELECT pr.factory, pr.product_name
+,pr_count.count
+FROM products pr
+LEFT JOIN (SELECT factory, COUNT(*) FROM products
+GROUP BY factory) pr_count ON pr.factory = pr_count.factory
+ORDER BY pr.factory, pr.product_name;
 
-
+SELECT pr.factory, pr.product_name
+,pr_count.count
+FROM ( SELECT pr.factory, pr.product_name from products pr)
+pr
+LEFT JOIN (SELECT factory, COUNT(*) FROM products
+GROUP BY factory) pr_count ON pr.factory = pr_count.factory
+ORDER BY pr.factory, pr.product_name;
 -- ASSIGNMENT 3: Subqueries in the WHERE clause
 
--- View all products from Wicked Choccy's
+SELECT * FROM products;
 
+-- View all products from Wicked Choccy's
+SELECT * FROM products
+WHERE factory = 'Wicked Choccy''s';
 
 -- Return products where the unit price is less than
 -- the unit price of all products from Wicked Choccy's
+SELECT * FROM products
+WHERE unit_price < ALL(SELECT unit_price FROM products
+WHERE factory = 'Wicked Choccy''s');
+
+SELECT * FROM products
+WHERE unit_price > ALL(SELECT unit_price FROM products
+WHERE factory = 'Wicked Choccy''s');
 
 
 -- ASSIGNMENT 4: CTEs
 
 -- View the orders and products tables
-
+SELECT * FROM orders o
+INNER JOIN products p on o.product_id = p.product_id;
 
 -- Calculate the amount spent on each product, within each order
-
+SELECT o.order_id,SUM(o.units * p.unit_price) total_sales FROM orders o
+INNER JOIN products p on o.product_id = p.product_id
+GROUP BY order_id
+ORDER BY total_sales DESC;
 
 -- Return all orders over $200
 
+WITH order_sales AS( SELECT o.order_id,
+							SUM(o.units * p.unit_price) total_sales 
+					  FROM orders o
+					  INNER JOIN products p on o.product_id = p.product_id
+					  GROUP BY order_id
+					  )
+SELECT *
+FROM order_sales os
+WHERE os.total_sales > 200;
+
+SELECT o.order_id,
+							SUM(o.units * p.unit_price) total_sales 
+FROM orders o
+INNER JOIN products p on o.product_id = p.product_id
+GROUP BY order_id
+HAVING  SUM(o.units * p.unit_price) > 200;
 
 -- Return the number of orders over $200
 
+WITH order_sales AS( SELECT o.order_id,
+							SUM(o.units * p.unit_price) total_sales 
+					  FROM orders o
+					  INNER JOIN products p on o.product_id = p.product_id
+					  GROUP BY order_id
+					  HAVING  SUM(o.units * p.unit_price) > 200
+					  )
+SELECT COUNT(*) total_orders_over_200
+FROM order_sales os;
 
 -- ASSIGNMENT 5: Multiple CTEs
 
 -- Copy over Assignment 2 (Subqueries in the FROM clause) solution
 
+SELECT factory, COUNT(*) FROM products
+GROUP BY factory;
 
 -- Rewrite the Assignment 2 subquery solution using CTEs instead
+WITH pr_count as(SELECT factory, COUNT(*) FROM products
+GROUP BY factory)
+SELECT pr.factory, pr.product_name, pr_count.count
+FROM products pr
+LEFT JOIN pr_count on pr.factory = pr_count.factory
+ORDER BY pr.factory, pr.product_name;
+
+WITH pr as (SELECT pr.factory, pr.product_name from products pr),
+pr_count as(SELECT factory, COUNT(*) FROM products
+GROUP BY factory)
+SELECT pr.factory, pr.product_name, pr_count.count
+FROM pr
+LEFT JOIN pr_count on pr.factory = pr_count.factory
+ORDER BY pr.factory, pr.product_name;
--- Subqueries & CTE demo.sql
+++ Subqueries & CTE demo.sql
@@ -194,3 +194,313 @@ FROM
 	) TOTAL_HS
 WHERE
 	TOTAL_HS.HAPPINESS_SCORE > TOTAL_HS.AVG_HPY_SC + 1;
+
+-- 4. Subqueries in the WHERE and HAVING clauses
+
+-- Average happiness score
+SELECT AVG(happiness_score) FROM happiness_scores;
+
+-- Above average happiness scores (WHERE)
+
+SELECT country,"year", happiness_score  FROM happiness_scores
+WHERE happiness_score > (SELECT AVG(happiness_score) FROM happiness_scores);
+
+-- Above average happiness scores for each region (HAVING)
+
+SELECT region, AVG(happiness_score) AVG_HS  FROM happiness_scores
+GROUP BY region
+HAVING AVG(happiness_score) > (SELECT AVG(happiness_score) FROM happiness_scores);
+
+-- 5. ANY vs ALL
+
+SELECT * FROM happiness_scores; --2015 - 2023
+SELECT * FROM happiness_scores_current; -- 2024
+
+-- Scores that are greater than ANY 2024 scores
+-- without any or all keywords
+SELECT * FROM happiness_scores
+WHERE happiness_score > (SELECT MIN(ladder_score) FROM happiness_scores_current);
+
+
+SELECT * FROM happiness_scores
+WHERE happiness_score > ANY(SELECT ladder_score FROM happiness_scores_current);
+
+-- Scores that are greater than ALL 2024 scores
+
+SELECT * FROM happiness_scores
+WHERE happiness_score > ALL(SELECT ladder_score FROM happiness_scores_current);
+
+
+-- 6. EXISTS
+
+SELECT * FROM happiness_scores;
+SELECT * FROM inflation_rates;
+
+/* Return happiness scores of countries
+that exist in the inflation rates table */
+SELECT * FROM happiness_scores hs
+WHERE EXISTS (
+	SELECT ir.country_name
+	FROM inflation_rates ir
+	WHERE hs.country = ir.country_name
+);
+
+SELECT * FROM happiness_scores hs
+WHERE EXISTS (
+	SELECT 1
+	FROM inflation_rates ir
+	WHERE hs.country = ir.country_name
+);
+
+-- Alternative to EXISTS: INNER JOIN
+SELECT * FROM happiness_scores hs
+INNER JOIN inflation_rates ir
+	ON hs.country = ir.country_name
+	AND hs."year" = ir."year";
+
+-- 7. CTEs: Readability
+
+/* SUBQUERY: Return the happiness scores along with
+   the average happiness score for each country */
+
+SELECT "year", hs.country, hs.happiness_score,
+c_hs.AVG_HS_BY_COUNTRY,
+hs.happiness_score - c_hs.avg_hs_by_country DIFF_AVG
+FROM happiness_scores hs
+LEFT JOIN (SELECT country, AVG(happiness_score) AVG_HS_BY_COUNTRY
+FROM happiness_scores
+GROUP BY country) c_hs
+ON hs.country = c_hs.country;
+
+/* CTE: Return the happiness scores along with
+   the average happiness score for each country */
+WITH c_hs AS(SELECT country, 
+					AVG(happiness_score) AVG_HS_BY_COUNTRY
+			FROM happiness_scores
+			GROUP BY country)
+SELECT "year", hs.country, hs.happiness_score,
+c_hs.AVG_HS_BY_COUNTRY,
+hs.happiness_score - c_hs.avg_hs_by_country DIFF_AVG
+FROM happiness_scores hs
+LEFT JOIN  c_hs
+ON hs.country = c_hs.country;
+
+-- 8. CTEs: Reusability
+        
+-- SUBQUERY: Compare the happiness scores within each region in 2023
+SELECT * FROM happiness_scores WHERE year = 2023;
+
+SELECT	hs1.region, hs1.country, hs1.happiness_score,
+		hs2.country, hs2.happiness_score
+FROM	happiness_scores hs1 INNER JOIN happiness_scores hs2
+		ON hs1.region = hs2.region;
+        
+SELECT	hs1.region, hs1.country, hs1.happiness_score,
+		hs2.country, hs2.happiness_score
+FROM	(SELECT * FROM happiness_scores WHERE year = 2023) hs1
+		INNER JOIN
+        (SELECT * FROM happiness_scores WHERE year = 2023) hs2
+		ON hs1.region = hs2.region;
+
+-- CTE: Compare the happiness scores within each region in 2023
+WITH hs AS (SELECT * FROM happiness_scores WHERE year = 2023)
+
+SELECT	hs1.region, hs1.country, hs1.happiness_score,
+		hs2.country, hs2.happiness_score
+FROM	hs hs1 INNER JOIN hs hs2
+		ON hs1.region = hs2.region
+WHERE	hs1.country < hs2.country;
+
+-- 8. CTEs: Reusability
+        
+-- SUBQUERY: Compare the happiness scores within each region in 2023
+SELECT * FROM happiness_scores WHERE year = 2023;
+
+SELECT hs1.region, hs1.country, hs1.happiness_score,
+		hs2.country, hs2.happiness_score
+FROM happiness_scores hs1
+INNER JOIN happiness_scores hs2 
+	ON hs1.region = hs2.region
+WHERE hs1.year = 2023;
+        
+SELECT hs1.region, hs1.country, hs1.happiness_score,
+		hs2.country, hs2.happiness_score 
+FROM (SELECT * FROM happiness_scores WHERE year = 2023) hs1
+INNER JOIN (SELECT * FROM happiness_scores WHERE year = 2023) hs2 
+			ON hs1.region = hs2.region;
+
+-- CTE: Compare the happiness scores within each region in 2023
+
+WITH hs AS(
+			SELECT * FROM happiness_scores WHERE year = 2023
+)
+SELECT hs1.region, hs1.country, hs1.happiness_score,
+		hs2.country, hs2.happiness_score
+		FROM hs hs1
+	INNER JOIN hs hs2 ON hs1.region = hs2.region
+	WHERE hs1.country < hs2.country;
+
+-- 9. Multiple CTEs
+
+-- Step 1: Compare 2023 vs 2024 happiness scores side by side
+WITH hs_2023 AS( SELECT * FROM happiness_scores 
+				  WHERE year = 2023),
+	 hs_2024 AS( SELECT * FROM happiness_scores_current)
+SELECT hs_2023.country, hs_2023.happiness_score hs_2023_score,
+hs_2024.ladder_score hs_2024_score
+FROM hs_2023 
+INNER JOIN hs_2024 on hs_2023.country = hs_2024.country;
+
+-- Step 2: Return the countries where the score increased
+
+WITH hs_2023 AS( SELECT * FROM happiness_scores 
+				  WHERE year = 2023),
+	 hs_2024 AS( SELECT * FROM happiness_scores_current)
+SELECT country, hs_2023_score, hs_2024_score
+FROM
+(SELECT hs_2023.country, hs_2023.happiness_score hs_2023_score,
+hs_2024.ladder_score hs_2024_score
+FROM hs_2023 
+INNER JOIN hs_2024 on hs_2023.country = hs_2024.country) hs_2023_2024
+WHERE hs_2024_score > hs_2023_score;
+
+-- Alternative: CTEs only
+
+WITH hs_2023 AS( SELECT * FROM happiness_scores 
+				  WHERE year = 2023),
+	 hs_2024 AS( SELECT * FROM happiness_scores_current),
+	 hs_2023_2024 AS(SELECT hs_2023.country, hs_2023.happiness_score hs_2023_score,
+hs_2024.ladder_score hs_2024_score
+FROM hs_2023 
+INNER JOIN hs_2024 on hs_2023.country = hs_2024.country)
+SELECT country, hs_2023_score, hs_2024_score
+FROM
+ hs_2023_2024
+WHERE hs_2024_score > hs_2023_score
+
+-- Step 3: Return the countries where the score decreased
+
+WITH hs_2023 AS( SELECT * FROM happiness_scores 
+				  WHERE year = 2023),
+	 hs_2024 AS( SELECT * FROM happiness_scores_current),
+	 hs_2023_2024 AS(SELECT hs_2023.country, hs_2023.happiness_score hs_2023_score,
+hs_2024.ladder_score hs_2024_score
+FROM hs_2023 
+INNER JOIN hs_2024 on hs_2023.country = hs_2024.country)
+SELECT country, hs_2023_score, hs_2024_score
+FROM
+ hs_2023_2024
+WHERE hs_2024_score < hs_2023_score;
+
+-- 10. Recursive CTEs
+
+-- Create a stock prices table
+CREATE TABLE IF NOT EXISTS stock_prices (
+    date DATE PRIMARY KEY,
+    price DECIMAL(10, 2)
+);
+
+INSERT INTO stock_prices (date, price) VALUES
+	('2024-11-01', 678.27),
+	('2024-11-03', 688.83),
+	('2024-11-04', 645.40),
+	('2024-11-06', 591.01);
+    
+/* Employee table was created in prior section:
+   This is the code if you need to create it again */
+    
+/*
+CREATE TABLE IF NOT EXISTS employees (
+    employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(100),
+    salary INT,
+    manager_id INT
+);
+
+INSERT INTO employees (employee_id, employee_name, salary, manager_id) VALUES
+	(1, 'Ava', 85000, NULL),
+	(2, 'Bob', 72000, 1),
+	(3, 'Cat', 59000, 1),
+	(4, 'Dan', 85000, 2);
+*/
+
+-- Example 1: Generating sequences
+SELECT * FROM stock_prices;
+
+-- Generate a column of dates
+WITH RECURSIVE my_dates(dt) AS
+	(SELECT '2024-11-01'
+     UNION ALL
+     SELECT dt + INTERVAL 1 DAY
+     FROM my_dates
+     WHERE dt < '2024-11-06')
+     
+SELECT * FROM my_dates;
+
+-- Include the original prices
+WITH RECURSIVE my_dates(dt) AS
+	(SELECT '2024-11-01'
+     UNION ALL
+     SELECT dt + INTERVAL 1 DAY
+     FROM my_dates
+     WHERE dt < '2024-11-06')
+     
+SELECT	md.dt, sp.price
+FROM	my_dates md
+		LEFT JOIN stock_prices sp
+        ON md.dt = sp.date;
+
+-- Example 2: Working with hierachical data
+SELECT * FROM employees;
+
+-- Return the reporting chain for each employee
+WITH RECURSIVE employee_hierarchy AS (
+    SELECT	employee_id, employee_name, manager_id,
+			employee_name AS hierarchy
+    FROM	employees
+    WHERE	manager_id IS NULL
+    
+    UNION ALL
+    
+    SELECT	e.employee_id, e.employee_name, e.manager_id,
+			CONCAT(eh.hierarchy, ' > ', e.employee_name) AS hierarchy
+    FROM	employees e INNER JOIN employee_hierarchy eh
+			ON e.manager_id = eh.employee_id
+)
+
+SELECT	employee_id, employee_name,
+		manager_id, hierarchy
+FROM	employee_hierarchy
+ORDER BY employee_id;
+
+-- 11. Subquery vs CTE vs Temp Table vs View
+
+-- Subquery
+SELECT * FROM
+
+(SELECT	year, country, happiness_score FROM happiness_scores
+UNION ALL
+SELECT	2024, country, ladder_score FROM happiness_scores_current) AS my_subquery;
+
+-- CTE
+WITH my_cte AS (SELECT	year, country, happiness_score FROM happiness_scores
+				UNION ALL
+				SELECT	2024, country, ladder_score FROM happiness_scores_current)
+                
+SELECT * FROM my_cte;
+
+-- Temporary table
+CREATE TEMPORARY TABLE my_temp_table AS
+SELECT	year, country, happiness_score FROM happiness_scores
+UNION ALL
+SELECT	2024, country, ladder_score FROM happiness_scores_current;
+
+SELECT * FROM my_temp_table;
+
+-- View
+CREATE VIEW my_view AS
+SELECT	year, country, happiness_score FROM happiness_scores
+UNION ALL
+SELECT	2024, country, ladder_score FROM happiness_scores_current;
+
+SELECT * FROM my_view;
